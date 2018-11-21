0e136fb20ce944bcfa59c7ec1bc493a82b81a09b
sigilai <briansigilai@gmail.com>
2018-11-21 18:24:12 +0300

    addeed volley and twigafoods mpesa library

--- app/src/main/java/com/example/sigilai/kotlin_ecommerce/MainActivity.kt
+++ app/src/main/java/com/example/sigilai/kotlin_ecommerce/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.sigilai.kotlin_ecommerce
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        signup_btn.setOnClickListener {
+
+            var intent = Intent(this,RegistrationActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
